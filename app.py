1a045a9001e282b497be8ef7d8f046976015cf09
Maki <sunwood.ai.labs@gmail.com>
2024-04-24 19:39:33 +0900

    [feat] フロントページの作成
    - `app.py`にフロントページの表示機能を追加
     - `load_markdown`関数を定義し、Markdownファイルを読み込む
     - `display_front_page`関数を定義し、`docs/page_front.md`を読み込んで表示する
    - `docs/page_front.md`を新規作成
     - プロジェクトのタイトル、アイコン、説明文を記述
     - GitHubのレポジトリ情報を表示するバッジを追加

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
+
+def load_markdown(file_path):
+    with open(file_path, encoding="utf8") as f:
+        return f.read()
+
+def display_front_page():
+    html_front = load_markdown('docs/page_front.md')
+    st.markdown(f"{html_front}", unsafe_allow_html=True)
+    
+    
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
