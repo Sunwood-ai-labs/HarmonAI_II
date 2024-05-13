95654271468a5c7d0ea27b921eba4156a34c595b
Maki <sunwood.ai.labs@gmail.com>
2024-05-13 22:07:30 +0900

    🔧 [feat] `HarmonAI`クラスにSNSバッジ生成機能を追加
    - `HarmonAI`クラスに新しい静的メソッド`generate_sns_badges`を追加し、SNSリンク用のバッジを生成する機能を実装しました。これにより、READMEにソーシャルメディアリンクを動的に挿入することが可能になります。
    - README生成機能をアップデートし、新たに追加されたSNSバッジを組み込むようにしました。

--- harmon_ai/harmon_ai.py
+++ harmon_ai/harmon_ai.py
@@ -1,12 +1,26 @@
 import os
 
-class harmon_ai:
+class HarmonAI:
     @staticmethod
-    def generate_readme(repo_name, owner_name, package_name, icon_url, title, subtitle, important_message, sections_content):
+    def generate_readme(
+        repo_name,
+        owner_name,
+        package_name,
+        icon_url,
+        title,
+        subtitle,
+        important_message,
+        sections_content,
+        website_url,
+        github_url, 
+        twitter_url,
+        blog_url
+    ):
         with open(os.path.join(os.path.dirname(__file__), "templates", "readme_template.md"), "r") as file:
             template = file.read()
 
-        badge_template = harmon_ai.generate_badges(repo_name, owner_name, package_name)
+        badge_template = HarmonAI.generate_badges(repo_name, owner_name, package_name)
+        sns_badges = HarmonAI.generate_sns_badges(website_url, github_url, twitter_url, blog_url)
 
         template = template.replace("{repo_name}", repo_name)
         template = template.replace("{owner_name}", owner_name)
@@ -17,6 +31,7 @@ class harmon_ai:
         template = template.replace("{badges}", badge_template)
         template = template.replace("{important_message}", important_message)
         template = template.replace("{sections_content}", sections_content)
+        template = template.replace("{sns_badges}", sns_badges)
 
         return template
 
@@ -28,5 +43,17 @@ class harmon_ai:
         template = template.replace("{repo_name}", repo_name)
         template = template.replace("{owner_name}", owner_name)
         template = template.replace("{package_name}", package_name)
+
+        return template
+
+    @staticmethod
+    def generate_sns_badges(website_url, github_url, twitter_url, blog_url):
+        with open(os.path.join(os.path.dirname(__file__), "templates", "sns_template.md"), "r") as file:
+            template = file.read()
+
+        template = template.replace("{website_url}", website_url)
+        template = template.replace("{github_url}", github_url)
+        template = template.replace("{twitter_url}", twitter_url)
+        template = template.replace("{blog_url}", blog_url)
 
         return template
