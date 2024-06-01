e3941908fcb64dc36e77c0e39632e6c55e75c566
Maki <sunwood.ai.labs@gmail.com>
2024-06-01 15:42:32 +0900

    🚀 [refactor] CICDファイル管理の改善と動的URL置換
    - `copy_cicd_file_if_missing`関数に`output_dir`パラメータを追加し、CICDファイルの管理を改善しました。開発用と本番用のCICDファイルパスを動的に扱うように調整し、パッケージ名に基づいてPyPI URLを動的に置換する機能を追加しました。これにより、環境ごとに適切な設定を適用できるようになります。

--- harmon_ai/utils.py
+++ harmon_ai/utils.py
@@ -26,14 +26,27 @@ def load_file_content(user_file_path, default_file_path):
     with open(user_path, "r", encoding="utf8") as file:
         return file.read()
 
-def copy_cicd_file_if_missing(config):
-    cicd_file_path = Path(config.get('cicd_file_path', './.harmon_ai/publish-to-pypi.yml'))
+def copy_cicd_file_if_missing(config, output_dir):
+    cicd_file_path = Path(output_dir) / config.get('cicd_file_path', 'publish-to-pypi.yml')
+    cicd_main_path = Path(config["production_dir"]) /  Path(config["github_cicd_dir"]) / config.get('cicd_main_path', 'publish-to-pypi.yml')
     template_path = Path(__file__).parent / config.get('cicd_template_path', 'templates/publish-to-pypi.yml')
     if not cicd_file_path.exists():
-        if not cicd_file_path.parent.exists():
-            os.makedirs(cicd_file_path.parent)
         shutil.copy(template_path, cicd_file_path)
-        logger.info("CICDファイルがテンプレートからコピーされました：{}", cicd_file_path)
+        logger.info("[Development] CICDファイルがテンプレートからコピーされました：{}", cicd_file_path)
+
+        # ファイルを読み込んで内容を置換
+        with open(cicd_file_path, 'r+', encoding='utf-8') as file:
+            content = file.read()
+            content = content.replace("https://pypi.org/p/harmon_ai", f"https://pypi.org/p/{config['package_name']}")
+            file.seek(0)
+            file.write(content)
+            file.truncate()  # 既存の内容が新しい内容より長い場合、余分な部分を削除
+            logger.info(f"[Development] URLが置換されました：{cicd_file_path}")
+
+    if not cicd_main_path.exists():
+        os.makedirs(cicd_main_path.parent, exist_ok=True)
+        shutil.copy(cicd_file_path, cicd_main_path)
+        logger.info("[Production] CICDファイルが開発用からコピーされました：{}", cicd_main_path)
 
 def config_preview(config):
     for key, value in config.items():
