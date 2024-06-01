697f79e36123bdd74e181573e49789c2eb295739
Maki <sunwood.ai.labs@gmail.com>
2024-06-01 18:46:21 +0900

    🔧 [refactor] harmon_ai/cli.pyのモジュールインポートとロギング設定を整理
    - 不要なインポートを削除し、必要なもののみを含める
    - `logger.configure` の設定を更新
    - `main` 関数をシンプル化し、`HarmonAI` インスタンスの生成と実行を行う

--- harmon_ai/cli.py
+++ harmon_ai/cli.py
@@ -1,12 +1,8 @@
 # main.py
-import os
-from pathlib import Path
-from .harmon_ai import HarmonAI
-from art import *
-from termcolor import colored
-from loguru import logger
 import sys
-from .utils import load_config, load_file_content, copy_cicd_file_if_missing, config_preview
+from .harmon_ai import HarmonAI
+from loguru import logger
+import os
 
 logger.configure(
     handlers=[
@@ -19,37 +15,9 @@ logger.configure(
 )
 
 def main():
-
-    config_path = os.path.expanduser("./.harmon_ai/config.yml")
-    config = load_config(config_path, os.path.join(Path(__file__).parent, "templates/config.example.yml"))
-
-    tprint("-- HarmonAI --")
-    config_preview(config)
-
-    output_dir = config['output_dir']
-    os.makedirs(output_dir, exist_ok=True)
-
-    important_message = load_file_content(
-        os.path.join(output_dir, config['important_message_file']),
-        "templates/important_template.md"
-    )
-    sections_content = load_file_content(
-        os.path.join(output_dir, config['sections_content_file']),
-        "templates/sections_template.md"
-    )
-
-    # CICDファイルを確認してコピー
-    copy_cicd_file_if_missing(config, output_dir)
-
-    # HarmonAIの使用例
-    readme_template = HarmonAI.run(config)
-
-    output_path = os.path.join(output_dir, config['output_file'])
-    with open(output_path, "w", encoding="utf8") as file:
-        file.write(readme_template)
-
-    logger.success("READMEファイルが生成されました：{}", output_path)
-    tprint("!! successfully !!")
+    harmon_ai = HarmonAI()
+    harmon_ai.run()
+    logger.success("READMEファイルが生成されました：{}", os.path.join(harmon_ai.config['harmon_ai']['development']['output_dir'], harmon_ai.config['harmon_ai']['product']['output_file']))
 
 if __name__ == "__main__":
     main()
