b6c39e1a93465bdd14007b3785ac189b68809773
Maki <sunwood.ai.labs@gmail.com>
2024-06-01 14:21:52 +0900

    🔧 [refactor] CLI構成管理とロギング機能を大幅に強化
    - argparseを削除し、yamlでの設定ファイル管理に移行しました。これにより設定管理が柔軟になります。
    - エラー処理とログ出力のためにloguruを導入しました。これはエラートラッキングとデバッグを容易にするためです。
    - ファイル操作をPathlibとshutilを用いて強化し、コードの可読性と安全性を向上させました。
    - 設定ファイルが存在しない場合の自動生成機能を追加し、ユーザビリティを高めました。

--- harmon_ai/cli.py
+++ harmon_ai/cli.py
@@ -1,79 +1,103 @@
 import os
-import argparse
+import shutil
+import yaml
 from .harmon_ai import HarmonAI
 from art import *
-import os
 from termcolor import colored
+from pathlib import Path
+from loguru import logger
+import sys
 
+logger.configure(
+    handlers=[
+        {
+            "sink": sys.stderr,
+            "format": "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level:<8}</level> | <cyan>{name:<45}:{line:<5}</cyan> | <level>{message}</level>",
+            "colorize": True,
+        }
+    ]
+)
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="GitHubリポジトリ用のREADMEテンプレートとバッジを生成します。")
-    parser.add_argument("--repo_name", help="GitHubリポジトリの名前", default="Repo Name")
-    parser.add_argument("--owner_name", help="リポジトリオーナーのGitHubユーザー名", default="Owner name")
-    parser.add_argument("--package_name", help="パッケージの名前", default="Package Name")
-    parser.add_argument("--icon_url", help="READMEに表示するアイコンのURL", default="Icon URL")
-    parser.add_argument("--title", help="プロジェクトのタイトル", default="Title")
-    parser.add_argument("--subtitle", help="プロジェクトのサブタイトル", default="Subtitle")
-    parser.add_argument("--important_message", help="READMEに表示する重要なメッセージ", default=None)
-    parser.add_argument("--sections_content", help="READMEのセクションの内容（introduction, demo, getting_started, updates, contributing, license, acknowledgements）をマークダウン形式で指定", default=None)
-    parser.add_argument("--website_url", help="プロジェクトのWebサイトのURL", default="")
-    parser.add_argument("--github_url", help="GitHubリポジトリのURL", default="")
-    parser.add_argument("--twitter_url", help="TwitterプロフィールのURL", default="")
-    parser.add_argument("--blog_url", help="公式ブログ記事のURL", default="")
-    parser.add_argument("--output_dir", help="出力ディレクトリ", default=".harmon_ai")
-    parser.add_argument("--output_file", help="出力ファイル名", default="_README_template.md")
+def load_config(file_path, default_path):
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        shutil.copy(default_path, file_path)
+        logger.info("設定ファイルがテンプレートから作成されました：{}", file_path)
+    with open(file_path, "r", encoding='utf8') as file:
+        return yaml.safe_load(file)
 
-    args = parser.parse_args()
-    return args, parser
+def load_file_content(user_file_path, default_file_path):
+    user_path = Path(user_file_path)
+    base_path = Path(__file__).parent
+    default_path = base_path / default_file_path
+
+    logger.debug("デフォルトパス：{}", default_path)
+    logger.debug("ユーザーパス：{}... 存在する：{}", user_path, os.path.exists(user_path))
+    
+    if not os.path.exists(user_path):
+        os.makedirs(user_path.parent, exist_ok=True)
+        shutil.copy(default_path, user_path)
+        logger.info("テンプレートファイルがこちらにコピーされました：{}", user_path)
+
+    with open(user_path, "r", encoding="utf8") as file:
+        return file.read()
 
 def main():
-    args, parser = parse_arguments()
-   
+    config_path = os.path.expanduser("./.harmon_ai/config.yml")
+    default_config_path = os.path.join(Path(__file__).parent, "templates/config.example.yml")
+    config = load_config(config_path, default_config_path)
+
     tprint("-- HarmonAI --")
-    
-    print(colored("Repository Name: ", "cyan") + colored(args.repo_name, "yellow"))
-    print(colored("Owner Name: ", "cyan") + colored(args.owner_name, "yellow"))
-    print(colored("Package Name: ", "cyan") + colored(args.package_name, "yellow"))
-    print(colored("Icon URL: ", "cyan") + colored(args.icon_url, "yellow"))
-    print(colored("Title: ", "cyan") + colored(args.title, "yellow"))
-    print(colored("Subtitle: ", "cyan") + colored(args.subtitle, "yellow"))
-    print(colored("Website URL: ", "cyan") + colored(args.website_url, "yellow"))
-    print(colored("GitHub URL: ", "cyan") + colored(args.github_url, "yellow"))
-    print(colored("Twitter URL: ", "cyan") + colored(args.twitter_url, "yellow"))
-    print(colored("Blog URL: ", "cyan") + colored(args.blog_url, "yellow"))
-    print(colored("Output Directory: ", "cyan") + colored(args.output_dir, "yellow"))
-    print(colored("Output File: ", "cyan") + colored(args.output_file, "yellow"))
-    
-    if args.important_message is None:
-        with open("./harmon_ai/templates/important_template.md", "r", encoding="utf8") as file:
-            args.important_message = file.read()
 
-    if args.sections_content is None:
-        with open("./harmon_ai/templates/sections_template.md", "r", encoding="utf8") as file:
-            args.sections_content = file.read()
-
-    readme_template = HarmonAI.run(
-        args.repo_name,
-        args.owner_name,
-        args.package_name,
-        args.icon_url,
-        args.title,
-        args.subtitle,
-        args.important_message,
-        args.sections_content,
-        args.website_url,
-        args.github_url,
-        args.twitter_url,
-        args.blog_url
+    important_message = load_file_content(
+        config['important_message_file'],
+        "templates/important_template.md"
+    )
+    sections_content = load_file_content(
+        config['sections_content_file'],
+        "templates/sections_template.md"
     )
 
-    os.makedirs(args.output_dir, exist_ok=True)
-    output_path = os.path.join(args.output_dir, args.output_file)
+    logger.info("リポジトリ名：{}", config['repo_name'])
+    logger.info("オーナー名：{}", config['owner_name'])
+    logger.info("パッケージ名：{}", config['package_name'])
+    logger.info("アイコンURL：{}", config['icon_url'])
+    logger.info("プロジェクトタイトル：{}", config['title'])
+    logger.info("サブタイトル：{}", config['subtitle'])
+    logger.info("重要なメッセージテンプレートファイル：{}", config['important_message_file'])
+    logger.info("セクション内容テンプレートファイル：{}", config['sections_content_file'])
+    logger.info("プロジェクトWebサイトURL：{}", config['website_url'])
+    logger.info("GitHub URL：{}", config['github_url'])
+    logger.info("Twitter URL：{}", config['twitter_url'])
+    logger.info("ブログURL：{}", config['blog_url'])
+    logger.info("出力ディレクトリ：{}", config['output_dir'])
+    logger.info("出力ファイル名：{}", config['output_file'])
+
+    # HarmonAIの使用例
+    readme_template = HarmonAI.run(
+        config['repo_name'],
+        config['owner_name'],
+        config['package_name'],
+        config['icon_url'],
+        config['title'],
+        config['subtitle'],
+        important_message,
+        sections_content,
+        config['website_url'],
+        config['github_url'],
+        config['twitter_url'],
+        config['blog_url']
+    )
+
+    output_dir = config['output_dir']
+    output_file = config['output_file']
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, output_file)
     with open(output_path, "w", encoding="utf8") as file:
         file.write(readme_template)
 
-    print(colored("READMEファイルが生成されました: ", "green") + colored(output_path, "yellow"))
+    logger.success("READMEファイルが生成されました：{}", output_path)
     tprint("!! successfully !!")
 
 if __name__ == "__main__":
-   main()
+    main()
